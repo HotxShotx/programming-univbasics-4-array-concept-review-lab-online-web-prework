aa41585989d7af52ca02a70459c1d8028a4926fb
outstanding-reference-3248 <omar.benjamin377@gmail.com>
2019-10-31 05:26:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,9 +6,16 @@ def find_element_index(array, value_to_find)
 
 
 def find_max_value(array)
-  # Add your solution here
+  max = 0
+  array.length.times do |count|
+    if array[count] > max
+      max = array[count]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = 0
+  array.length.times do |count|
+    if count == 0
+      min = array[count]
 end
+
